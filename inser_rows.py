ac0c81867ce49f3d6b0804116bec073cd7522f70
KaplySaipriya <kaplysaipriya@gmail.com>
2026-06-04 11:49:03 +0530

    Added MySQL connection and table creation scripts

--- inser_rows.py
+++ inser_rows.py
@@ -1,35 +1,4 @@
-import db_connect2 as dbc
 import pymysql
-
-
-def create_table():
-    query = """
-    CREATE TABLE IF NOT EXISTS priya_table (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        designation VARCHAR(255),
-        salary INT
-    )
-    """
-
-    try:
-        connection = dbc.db_connect()
-
-        if connection is None:
-            return
-
-        cursor = connection.cursor()
-
-        cursor.execute(query)
-        connection.commit()
-
-        print("Table Created Successfully")
-
-        cursor.close()
-        dbc.db_disconnect(connection)
-
-    except Exception as e:
-        print("Error While Creating Table:", e)
 
 def insert_row():
     query = """
@@ -64,7 +33,3 @@ def insert_row():
         print("Error:", e)
 
 insert_row()
-
-
-# Function Call
-create_table()
